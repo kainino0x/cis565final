b411d5ce4df9557db319711b8c49cfbdfa8f234f
JivingTechnostic <jiatong@seas.upenn.edu>
2014-12-08 03:05:16 -0500

    Removed stream compaction performance tests
    just assume they're all terrible.

--- src/js/fracturecl.js
+++ src/js/fracturecl.js
@@ -15,13 +15,13 @@ function index3(array, index) {
             array[4 * index + 1],
             array[4 * index + 2]];
 }
- var at0 = 0, at1 = 0, at2 = 0, atime1 = 0, atime2 = 0, atime3 = 0;
+
 function clInit() {
     var ctx = webcl.createContext(/*WebCL.DEVICE_TYPE_GPU*/);
     var kernelSrc = loadKernel("fracturecl");
     var program = ctx.createProgram(kernelSrc);
     var device = ctx.getInfo(WebCL.CONTEXT_DEVICES)[0];
-    console.log(device.getInfo(WebCL.DEVICE_NAME));
+    console.log(device.getInfo(WebCL.DEVICE_NAME)); 
 
     try {
         program.build([device], "");
@@ -223,24 +223,18 @@ function clOutputToInput(cl, oldtricount) {
     var scanSize = Math.pow(2.0, Math.ceil(Math.log2(oldtricount)));
     var globalWS = [Math.ceil(scanSize / localsize) * localsize];
     
-    at0 = performance.now();
     // we will be performing a naive double-buffered scan
     cl.scanpointoutA = cl.ctx.createBuffer(WebCL.MEM_READ_WRITE, scanSize * 4);
     cl.scanpointoutB = cl.ctx.createBuffer(WebCL.MEM_READ_WRITE, scanSize * 4);
-    at1 = performance.now();
     
     cl.scaninitkernel.setArg(0, new Uint32Array([oldtricount]));
     cl.scaninitkernel.setArg(1, cl.bufnewoutcells);
     cl.scaninitkernel.setArg(2, new Uint32Array([scanSize]));
     cl.scaninitkernel.setArg(3, cl.scanpointoutA);
     
-    at2 = performance.now();
     // this kernel fills cl.scantrioutA with 0's and 1's depending on the cellID in cl.buftrioutcells
     cl.queue.enqueueNDRangeKernel(cl.scaninitkernel, globalWS.length, null, globalWS, localWS);
     cl.queue.finish();
-    atime1 += performance.now() - at0;
-    atime2 += performance.now() - at1;
-    atime3 += performance.now() - at2;
     
     // debug
     /*
@@ -260,7 +254,6 @@ function clOutputToInput(cl, oldtricount) {
         }
     }*/
     // end debug
-    at0 = performance.now();
     var i = 0;  // keeps track of where the final result is.
     cl.scaniterkernel.setArg(1, new Uint32Array([scanSize]));
     // NOTE: used to have these alternate 2, 3 in the loop based on i % 2, but it seems like handling it in the kernel is faster for some reason.  needs further testing.
@@ -272,9 +265,7 @@ function clOutputToInput(cl, oldtricount) {
         cl.queue.enqueueNDRangeKernel(cl.scaniterkernel, globalWS.length, null, globalWS, localWS);
     }
     cl.queue.finish();
-    //atime2 += performance.now() - at0;
     
-    at0 = performance.now();
     // NOTE: we need to copy cl.scantrioutA/B in order to get the correct triangle count.  Is there a better way?
     var scanout = new Int32Array(scanSize);
     if (i % 2 == 0) {
@@ -309,7 +300,6 @@ function clOutputToInput(cl, oldtricount) {
         cl.queue.enqueueReadBuffer(cl.bufscatpointsout, false, 0, scatteroutpoints.byteLength, scatteroutpoints);
     }
     cl.queue.finish();
-    //atime3 += performance.now() - at0;
     // END STREAM COMPACTION FOR POINTS
     
     
@@ -644,9 +634,6 @@ function clFracture(cl, vertices, faces, rotation, pImpact) {
     console.log("        time_setupargs: "     + time_setupargs);
     console.log("        time_kernel: "        + time_kernel);
     console.log("        time_outputtoinput: " + time_outputtoinput);
-    console.log("           time_kernel1: " + atime1);
-    console.log("           time_kernel2: " + atime2);
-    console.log("           time_kernel3: " + atime3);
     console.log("        time_proximity: "     + time_proximity);
     console.log("    time_collect: "           + time_collect);
     console.log("    time_recenter: "          + time_recenter);
